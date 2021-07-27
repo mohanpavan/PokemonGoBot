aa8a73c852507859a048157b7af5794c2eab7889
Mohan krishna <mohanpavansh@gmail.com>
2021-07-28 00:20:01 +0530

    time-filter updated

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 import re
 import time
-import datetime;
+from datetime import datetime, timedelta
 
 pattern = re.compile('.*\((\d+%)\).*Level: ([3]\d).*')
 
@@ -17,7 +17,7 @@ channel_ids = {'trapinch': '416893323434000384',
 
 def check_iv(iv):
     iv_num = int(iv.rstrip('%'))
-    return True if iv_num > 80 else False
+    return True if iv_num > 70 else False
 
 
 def check_level(level):
@@ -26,6 +26,16 @@ def check_level(level):
 
 def pokemon_filtered(iv, level):
     return True if check_iv(iv) and check_level(level) else False
+
+def filter_on_time(current_time, msg_time):
+    datetimeObj = datetime.strptime(msg_time[:-6], '%Y-%m-%dT%H:%M:%S.%f')
+    datetimeObj += timedelta(hours=5, minutes=30) # change to IST
+    print('spawn-time:', datetimeObj)
+    diff = current_time - datetimeObj
+    if ((diff.seconds % 3600) // 60) < 19: # return true if time-difference is less than 19mins
+        return True
+    else:
+        False
 
 def retrieve_messages(channel_id, current_time):
     headers = {
@@ -37,10 +47,11 @@ def retrieve_messages(channel_id, current_time):
     for value in data1:
         match_data = pattern.match(value['content'])
         if match_data:
-            if pokemon_filtered(match_data.group(1), match_data.group(2)):
-                print(value['timestamp'], '\n\n', value['content'])
+            if pokemon_filtered(match_data.group(1), match_data.group(2)) and filter_on_time(current_time, value['timestamp']):
+                print('\n\n', value['content'])
 
-for channel, channel_id in channel_ids.items():
-    cur_time = datetime.datetime.now()
-    print(cur_time)
-    retrieve_messages(channel_id, cur_time)
+cur_time = datetime.now()
+print('current-time: ' , cur_time)
+retrieve_messages('416797870986100738', cur_time)
+# for channel, channel_id in channel_ids.items():
+#     retrieve_messages(channel_id, cur_time)
